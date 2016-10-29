e3e2ce5c5fca083220c5e5a95a1d29bae0aa9857
Andrey Alekseenko <al42and@gmail.com>
2016-10-29 14:21:02 +0300

    Minor code cleanup

--- qn.py
+++ qn.py
@@ -1,18 +1,25 @@
+# -*- encoding:utf-8
+
+import numpy as np
+import pandas as pd
+
+
 def qn(df_input):
     """
     df_input.shape = (n, p),
     where p -- number of genes
-    n -- number of probs
-    
+    n -- number of probes
     """
-    df = np.array(df_input.T)
-    idx = df.argsort(axis = 0)
 
-    df.sort(axis = 0)
-    
-    quantiles = np.mean(df, axis = 1)
+    df = np.array(df_input.T)
+    idx = df.argsort(axis=0)
+
+    df.sort(axis=0)
+
+    quantiles = np.mean(df, axis=1)
     idx_final = idx.argsort(axis=0)
-    
+
     res = pd.DataFrame(quantiles[idx_final].T, columns=df_input.columns, index=df_input.index)
-    
+
     return res
+
